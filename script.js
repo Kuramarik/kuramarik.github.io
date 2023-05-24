c9deb9e2a6b2662e55c2148bd2e6e95dabb848da
Ethan Kou <ethan.y.kou@gmail.com>
2023-05-23 19:39:25 -0700

    Now Registrations persist between loads

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 const buttons = document.querySelectorAll('button');
 
 buttons.forEach((button) => {
+    if(localStorage.getItem(button.getAttribute('id')) != null) {
+      button.innerHTML=localStorage.getItem(button.getAttribute('id'))
+    }
   button.addEventListener('click', function (e) {
-    if(e.target.classList.contains("signedin")) {
+    //assigning all buttons a click function
+    if(e.target.innerHTML != "Register") {
+      //if the button has a signed-in name, the option to unregister will appear
         if(confirm("Would you like to unregister?")) {
             e.target.innerHTML = "Register"
-            e.target.classList.remove("signedin")
+            localStorage.removeItem(e.target.getAttribute('id'))
         }
     }
     else {
+      //if not, the button will take a name and mark it signed-in
         const name = prompt("Please enter your full name");
         e.target.innerHTML = name
-        e.target.classList.add("signedin")
-        console.log(e.target.classList)
+        localStorage.setItem(e.target.getAttribute('id'), name)
     }
   });
 });
